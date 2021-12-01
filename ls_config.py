3d984fdd8f8a977dd26924d63f32fd4e3f7ef5b3
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2021-12-01 22:01:06 +0100

    remove print

--- ls_config.py
+++ ls_config.py
@@ -17,9 +17,6 @@ def getConfig(configFilename):
     rules = checkRules(variables, constants, axiom, rules, translations)
     translations = checkTranslations(variables, constants, axiom, rules, translations)
     
-    #TODO remove
-    print([variables, constants, axiom, rules, translations])
-    
     return variables, constants, axiom, rules, translations
 
 def getAxiomFromConfig(configDict):
