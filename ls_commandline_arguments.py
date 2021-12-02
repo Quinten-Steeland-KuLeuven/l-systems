323b29862c893ee5a81da37cb99df0c08fc02d91
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2021-12-02 18:23:37 +0100

    Add -s and -up option

--- ls_commandline_arguments.py
+++ ls_commandline_arguments.py
@@ -2,20 +2,22 @@
 from ls_user_input import getConfigFilename
 
 def processCommandlineArguments(allArguments):
-    configFilename = iterations = exportImageName = useRandomConfig = None
-    showDrawProcess = noProgressbar = closeAfterDrawing = useWebsite= False
+    configFilename = iterations = exportImageName = useRandomConfig = useProgressbar = None
+    showDrawProcess = closeAfterDrawing = useWebsite = exportSvg = False
     
     helpMessage = """
     All commandline options:
-    -h  or  --help                          Displays help.
-    -e  or  --export <filename>             Exports the turtle drawing to a file.
-    -c  or  --config <configfile name>      Used to give the config file name to the program.
-    -i  or  --iterations [amount]           Used to give the amount of iterations to the program.
-    -sd or  --show_draw_process             Use this flag to see the turtle move.
-    -np or  --no_progress_bar               Use this flag if you want no progressbar (e.g. for speed).
-    -ca or  --close_after_drawing           Exit immediately after drawing.
-    -rc or  --random_config                 Generate a random l-System
-    -uw or  --use_website                            Updates the website to the latest l-system
+    -h  or  --help                                  Displays help.
+    -c  or  --config <config filename>              Used to give the config file name to the program.
+    -i  or  --iterations [amount]                   Used to give the amount of iterations to the program.
+    -e  or  --export <filename>                     Exports the turtle drawing to an eps file.
+    -s  or  --output_svg                            Convert the eps image from eps to svg.
+    -sd or  --show_draw_process                     Use this flag to see the turtle move.
+    -up or  --use_progress_bar                      Use this flag if you always want a progressbar.
+    -np or  --no_progress_bar                       Use this flag if you want no progressbar (e.g. for speed).
+    -ca or  --close_after_drawing                   Exit immediately after drawing.
+    -rc or  --random_config <settings filename>     Generate a random l-System.
+    -uw or  --use_website                           Generate a website with the latest lSystem drawing.
     """
     
     counter = 0
@@ -78,8 +80,11 @@ def processCommandlineArguments(allArguments):
         elif arg == "-sd" or arg == "--show_draw_process":
             showDrawProcess = True
             
+        elif arg == "-up" or arg == "--use_progress_bar":
+            useProgressbar = True
+            
         elif arg == "-np" or arg == "--no_progress_bar":
-            noProgressbar = True
+            useProgressbar = False
             
         elif arg == "-ca" or arg == "--close_after_drawing":
             closeAfterDrawing = True
@@ -87,12 +92,18 @@ def processCommandlineArguments(allArguments):
         elif arg == "-uw" or arg == "--use_webiste":
             useWebsite = True
 
-        elif useWebsite and exportImageName == None:
-            exportImageName = ""
+        elif arg == "-s" or arg == "--output_svg":
+            exportSvg = True
 
         else:
             print(f"Unknown argument: '{arg}'  Use --help for help.")
             exit(0)
-    
-    return configFilename, iterations, exportImageName, showDrawProcess, noProgressbar, closeAfterDrawing, useRandomConfig, useWebsite
+            
+    if useWebsite and exportImageName == None:
+        exportImageName = ""
+          
+    if exportSvg:
+        exportImageName = ""
+        
+    return configFilename, iterations, exportImageName, showDrawProcess, useProgressbar, closeAfterDrawing, useRandomConfig, useWebsite, exportSvg
    
